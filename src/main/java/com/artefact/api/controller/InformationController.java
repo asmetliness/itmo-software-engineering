25a2b824cfe93ad1ec4edcbce1ae3ecf80f30b28
Naymoll <xdarkknight122x@gmail.com>
2022-06-21 00:26:59 +0300

    Начало реализации

--- src/main/java/com/artefact/api/controller/InformationController.java
+++ src/main/java/com/artefact/api/controller/InformationController.java
@@ -1,30 +1,51 @@
 package com.artefact.api.controller;
 
 import com.artefact.api.consts.OrderStatusIds;
+import com.artefact.api.consts.RoleNames;
 import com.artefact.api.model.Information;
-import com.artefact.api.repository.InformationRepository;
+import com.artefact.api.model.Role;
+import com.artefact.api.model.User;
+import com.artefact.api.repository.*;
 import com.artefact.api.request.CreateInformationOrder;
 import com.artefact.api.response.InformationResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Optional;
 
 import static org.springframework.security.core.context.SecurityContextHolder.getContext;
 
 @Controller
 @RequestMapping("/api/information")
 public class InformationController {
-    @Autowired
-    private InformationRepository infoRepository;
+    private final InformationRepository infoRepository;
+
+    private final UserRepository userRepository;
+
+    private final OrderStatusRepository orderStatusRepository;
+
+    private final RoleRepository roleRepository;
+
+    private final NotificationRepository notificationRepository;
+
+    public InformationController(InformationRepository infoRepository,
+                                 UserRepository userRepository,
+                                 OrderStatusRepository orderStatusRepository,
+                                 RoleRepository roleRepository,
+                                 NotificationRepository notificationRepository) {
+        this.infoRepository = infoRepository;
+        this.userRepository = userRepository;
+        this.orderStatusRepository = orderStatusRepository;
+        this.roleRepository = roleRepository;
+        this.notificationRepository = notificationRepository;
+    }
 
     @PostMapping
-    public ResponseEntity<Object> CreateOrder(@RequestBody CreateInformationOrder request) {
+    public ResponseEntity<Object> CreateInformation(@RequestBody CreateInformationOrder request) {
         String userId = (String) getContext().getAuthentication().getPrincipal();
 
         Information info = new Information();
@@ -39,6 +60,26 @@ public class InformationController {
 
         infoRepository.save(info);
 
+        return GetInformation(info.getId());
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<InformationResponse>> GetInformationList() {
+        String userId = (String) getContext().getAuthentication().getPrincipal();
+
+        Iterable<Information> information = infoRepository.getAll();
+        ArrayList<InformationResponse> response = new ArrayList<>();
+
+        for (Information info : information) {
+            InformationResponse info_response = (InformationResponse) GetInformation(info.getId()).getBody();
+            response.add(info_response);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Object> GetInformation(@PathVariable Long id) {
         InformationResponse response = null; // TODO: Добавить правильный ответ
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/main/java/com/artefact/api/repository/InformationRepository.java
+++ src/main/java/com/artefact/api/repository/InformationRepository.java
@@ -1,6 +1,7 @@
 package com.artefact.api.repository;
 
 import com.artefact.api.model.Information;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional
 public interface InformationRepository extends PagingAndSortingRepository<Information, Long> {
+
+    // TODO: Поменять на джоин таблиц. Пока захардкоржено
+    @Query("Select i from Information i where i.statusId = 1") // 1 = OrderStatusIds.NewOrder
+    Iterable<Information> getAll();
 }

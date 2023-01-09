b16b3ff64117730129119dd0ec1c5b95bc84d9ff
abramovich <abramovich@speechpro.com>
2023-01-09 10:13:33 +0300

    fix tests

--- src/test/java/com/artefact/api/DatabaseTests.java
+++ src/test/java/com/artefact/api/DatabaseTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -16,9 +17,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(locations="classpath:application-test.yml", properties = {
+@TestPropertySource(properties = {
 		"POSTGRESQL_DB_HOST=localhost"
 })
+@ActiveProfiles("test")
 
 class DatabaseTests {
 

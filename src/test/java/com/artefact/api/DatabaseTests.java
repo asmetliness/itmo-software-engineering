6970f0aa8282afc8e8872a3cf73d8b2b8304e80d
abramovich <abramovich@speechpro.com>
2023-01-09 09:56:07 +0300

    database tests init

---
+++ src/test/java/com/artefact/api/DatabaseTests.java
@@ -0,0 +1,33 @@
+package com.artefact.api;
+
+import com.artefact.api.repository.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations="classpath:application-test.yml", properties = {
+		"POSTGRESQL_DB_HOST=localhost"
+})
+
+class DatabaseTests {
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Test
+	void contextLoads() {
+		assertThat(orderRepository).isNotNull();
+	}
+
+}

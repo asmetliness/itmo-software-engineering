3b6c301312896500e1fec27662b3c84602a00f34
abramovich <abramovich@speechpro.com>
2023-01-09 20:14:43 +0300

    uopd

--- src/test/java/com/artefact/api/DatabaseTests.java
+++ src/test/java/com/artefact/api/DatabaseTests.java
@@ -1,6 +1,12 @@
 package com.artefact.api;
 
+import com.artefact.api.model.Artifact;
+import com.artefact.api.model.Order;
+import com.artefact.api.model.User;
 import com.artefact.api.repository.OrderRepository;
+import com.artefact.api.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +14,8 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -26,10 +34,24 @@ class DatabaseTests {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
 		assertThat(orderRepository).isNotNull();
+	}
+	
+
+
+	@Test()
+	void order_invalidArtifact() {
+
+		Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+			var order = new Order();
+			order.setArtifactId(10000L);
+			orderRepository.save(order);
+		});
 	}
 
 }
